46c5134a26f257989eae804eb0ea03d8dffd76b4
c0smosaur <sjkang539@gmail.com>
2024-05-23 16:38:55 +0900

    [FEAT] #18 엔티티 생성
    - Company
    - CompanyMembership, MemberCompanyMembership
    - IndividualMembership, MemberIndividualMembership
    - Reservation (+필요 enum)

---
+++ src/main/java/com/core/linkup/reservation/entity/Reservation.java
@@ -0,0 +1,37 @@
+package com.core.linkup.reservation.entity;
+
+import com.core.linkup.common.entity.BaseEntity;
+import com.core.linkup.reservation.entity.enums.ReservationStatus;
+import com.core.linkup.reservation.entity.enums.ReservationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "reservation")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class Reservation extends BaseEntity {
+
+    private Long memberId;
+    private Long indMembershipId;
+    private Long comMembershipId;
+    private Long seatId;
+
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Long price;
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus status;
+    @Enumerated(EnumType.STRING)
+    private ReservationType type;
+}
