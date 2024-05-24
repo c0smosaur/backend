b860a091939a844a0e4ea8786b30f0cd8f719290
c0smosaur <sjkang539@gmail.com>
2024-05-24 16:27:39 +0900

    [CHORE] JpaConfig 추가
    - JpaAuditing에 필요(누락됨)

---
+++ src/main/java/com/core/linkup/common/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.core.linkup.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
