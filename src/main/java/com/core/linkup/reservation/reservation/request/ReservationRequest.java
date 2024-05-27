58f79317e977be42408148eb26d547ac9bf47a4c
c0smosaur <sjkang539@gmail.com>
2024-05-27 10:32:02 +0900

    [FEAT] #28 예약 요청/응답 클래스 추가

--- src/main/java/com/core/linkup/reservation/reservation/request/ReservationRequest.java
+++ src/main/java/com/core/linkup/reservation/reservation/request/ReservationRequest.java
@@ -1,4 +1,16 @@
 package com.core.linkup.reservation.reservation.request;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class ReservationRequest {
+
+    private String type;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Long price;
+    private Long seatId;
+
 }
