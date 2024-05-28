179a9cb58af74871a5150a7eabb44cedaf588714
aelimited <cloim09@gmail.com>
2024-05-28 11:05:27 +0900

    [FIX] #30 - querydsl 적용 중
    - 코드 수정 중

---
+++ src/main/java/com/core/linkup/club/request/ClubRequestTest.java
@@ -0,0 +1,12 @@
+package com.core.linkup.club.request;
+
+public class ClubRequestTest {
+    private String category;
+    private boolean clubAccessibility;
+    private int recruitCount;
+    private String title;
+    private String introduction;
+    private String detailedIntroduction;
+    private String applicationIntroduction;
+    private String clubThumbnail;
+}
