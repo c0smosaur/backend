01f6a314959aae87ecc7dc3e92527b4deab6b04d
aelimited <cloim09@gmail.com>
2024-05-29 16:15:28 +0900

    [REMOVE] #30 - 사용하지 않는 파일 삭제
    - 소모임을 작업하면서 생성 했던 파일 중, 사용하지 않는 파일 삭제

--- src/main/java/com/core/linkup/club/repository/ClubRepository28.java
+++
@@ -1,9 +0,0 @@
-package com.core.linkup.club.repository;
-
-import com.core.linkup.club.entity.Club;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClubRepository28 extends JpaRepository<Club, Long> {
-}
