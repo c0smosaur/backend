992ae5cbb9441471d474b2257c848dbec73dc910
Seo Jean Kang <119040660+c0smosaur@users.noreply.github.com>
2024-05-31 04:37:01 +0900

    [FIX] Update RedisConfig.java
    Added configured RedisConnectionFactory bean

--- src/main/java/com/core/linkup/common/config/RedisConfig.java
+++ src/main/java/com/core/linkup/common/config/RedisConfig.java
@@ -1,13 +1,33 @@
 package com.core.linkup.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.redis.host}") // Load host and port from application.properties or application.yml
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}") // Load Redis password from application.properties or application.yml
+    private String redisPassword;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        config.setPassword(RedisPassword.of(redisPassword));
+        return new LettuceConnectionFactory(config);
+    }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
