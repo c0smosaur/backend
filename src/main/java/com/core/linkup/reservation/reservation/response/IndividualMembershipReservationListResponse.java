f113927e5fc07cc6ee9b76afcc8f638133a84dfd
c0smosaur <sjkang539@gmail.com>
2024-05-30 12:08:29 +0900

    [RENAME] #38 개인 멤버십 예약 조회 응답 이름 변경
    - 생성 이외에도 계속 사용할 형태이므로 이름 변경

--- src/main/java/com/core/linkup/reservation/reservation/response/IndividualMembershipReservationListResponse.java
+++ src/main/java/com/core/linkup/reservation/reservation/response/IndividualMembershipReservationListResponse.java
@@ -8,8 +8,12 @@ import java.util.List;
 
 @Builder
 @Getter
-public class IndividualMembershipRegistrationResponse {
+public class IndividualMembershipReservationListResponse {
 
+    // 개인 멤버십 예약 정보
+    // 개인 멤버십
+    // 예약(리스트)(좌석, 공간)
     private IndividualMembershipResponse membership;
     private List<ReservationResponse> reservations;
+
 }
