006c0803b98447e190867e3f9f8f64b63dd9e314
c0smosaur <sjkang539@gmail.com>
2024-05-31 00:57:30 +0900

    [RENAME] #52 응답형태 이름 변경
    - CompanyMembershipReservationResponse -> CompanyMembershipReservationListResponse

--- src/main/java/com/core/linkup/reservation/reservation/response/CompanyMembershipReservationListResponse.java
+++ src/main/java/com/core/linkup/reservation/reservation/response/CompanyMembershipReservationListResponse.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Builder
 @Getter
-public class CompanyMembershipReservationResponse {
+public class CompanyMembershipReservationListResponse {
 
     // 기업 멤버십 예약 정보
     // 기업 멤버십
