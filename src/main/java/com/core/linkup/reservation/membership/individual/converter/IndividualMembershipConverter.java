4ac93119bf1be3e2658f43332ebcc47188e52c1e
c0smosaur <sjkang539@gmail.com>
2024-05-30 12:13:15 +0900

    [FEAT] #38 개인 멤버십 생성/조회 기능
    - 사용자의 전체 개인멤버십 조회(예약까지 포함)
    - 사용자의 멤버십에 해당하는 전체 예약 조회

--- src/main/java/com/core/linkup/reservation/membership/individual/converter/IndividualMembershipConverter.java
+++ src/main/java/com/core/linkup/reservation/membership/individual/converter/IndividualMembershipConverter.java
@@ -1,12 +1,14 @@
 package com.core.linkup.reservation.membership.individual.converter;
 
 import com.core.linkup.common.annotation.Converter;
+import com.core.linkup.member.entity.Member;
 import com.core.linkup.reservation.membership.individual.entity.IndividualMembership;
 import com.core.linkup.reservation.membership.individual.response.IndividualMembershipResponse;
 
 @Converter
 public class IndividualMembershipConverter {
 
+    // 개인 멤버십 응답
     public IndividualMembershipResponse toIndividualMembershipResponse(
             IndividualMembership individualMembership) {
 
@@ -16,9 +18,9 @@ public class IndividualMembershipConverter {
                 .price(individualMembership.getPrice())
                 .type(individualMembership.getType().getName())
                 .duration(individualMembership.getDuration())
-                .startDate(individualMembership.getStartDate())
-                .endDate(individualMembership.getEndDate())
-                .memberId(individualMembership.getMember().getId())
+                .startDate(individualMembership.getStartDate().toLocalDate())
+                .endDate(individualMembership.getEndDate().toLocalDate())
+                .memberId(individualMembership.getMemberId())
                 .build();
 
     }
--- src/main/java/com/core/linkup/reservation/membership/individual/repository/IndividualMembershipRepository.java
+++ src/main/java/com/core/linkup/reservation/membership/individual/repository/IndividualMembershipRepository.java
@@ -3,5 +3,11 @@ package com.core.linkup.reservation.membership.individual.repository;
 import com.core.linkup.reservation.membership.individual.entity.IndividualMembership;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IndividualMembershipRepository extends JpaRepository<IndividualMembership, Long> {
+
+import java.util.List;
+
+public interface IndividualMembershipRepository extends JpaRepository<IndividualMembership, Long>,
+        IndividualMembershipRepositoryCustom {
+
+    List<IndividualMembership> findAllByMemberIdOrderByCreatedAtDesc(Long id);
 }
