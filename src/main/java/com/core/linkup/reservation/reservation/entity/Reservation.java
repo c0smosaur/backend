ab004a738e9d069594ed5e1ac8e9a7cf22f93543
c0smosaur <sjkang539@gmail.com>
2024-06-01 16:18:01 +0900

    [FEAT] #52 예약 조회 및 응답 변환 분리
    - 예약 주석 정리 및 @Setter 추가
    - 멤버십에 해당하는 예약 조회 시 예약+좌석 응답 변환

--- src/main/java/com/core/linkup/reservation/reservation/entity/Reservation.java
+++ src/main/java/com/core/linkup/reservation/reservation/entity/Reservation.java
@@ -1,17 +1,10 @@
 package com.core.linkup.reservation.reservation.entity;
 
 import com.core.linkup.common.entity.BaseEntity;
-import com.core.linkup.member.entity.Member;
-import com.core.linkup.office.entity.SeatSpace;
-import com.core.linkup.reservation.membership.company.entity.CompanyMembership;
-import com.core.linkup.reservation.membership.individual.entity.IndividualMembership;
 import com.core.linkup.reservation.reservation.entity.enums.ReservationStatus;
 import com.core.linkup.reservation.reservation.entity.enums.ReservationType;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
@@ -21,27 +14,20 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@SuperBuilder
+@SuperBuilder(toBuilder = true)
 @EqualsAndHashCode(callSuper = true)
 public class Reservation extends BaseEntity {
 
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private Long price;
+    @Setter
     @Enumerated(EnumType.STRING)
     private ReservationStatus status;
     @Enumerated(EnumType.STRING)
     private ReservationType type;
 
-//    @ManyToOne
-//    private CompanyMembership companyMembership;
     private Long companyMembershipId;
-
-//    @ManyToOne
-//    private IndividualMembership individualMembership;
     private Long individualMembershipId;
-
-//    @ManyToOne
-//    private SeatSpace seatSpace;
     private Long seatId;
 }
--- src/main/java/com/core/linkup/reservation/reservation/repository/ReservationRepositoryCustom.java
+++ src/main/java/com/core/linkup/reservation/reservation/repository/ReservationRepositoryCustom.java
@@ -1,18 +1,32 @@
 package com.core.linkup.reservation.reservation.repository;
 
 
+import com.core.linkup.office.entity.SeatSpace;
 import com.querydsl.core.Tuple;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReservationRepositoryCustom {
 
-    // 사용자의 전체 멤버십/예약/좌석
+    // 사용자의 전체 개인 멤버십/예약/좌석 조회
     List<Tuple> findAllIndividualMembershipAndReservationsAndSeatByMemberId(Long memberId);
 
-    // 사용자의 해당 지점 전체 멤버십/예약/좌석
+    // 사용자의 해당 지점 전체 개인 멤버십/예약/좌석 조회
     List<Tuple> findAllReservationsAndSeatAndIndividualMembershipByMemberIdAndOfficeId(Long memberId, Long officeId);
 
-    // 특정 멤버십의 전체 예약/좌석
-    List<Tuple> findAllReservationAndSeatByMembershipId(Long membershipId, Long memberId);
+    // 특정 개인 멤버십의 전체 예약/좌석
+    List<Tuple> findAllReservationAndSeatByIndividualMembershipId(Long membershipId, Long memberId);
+
+    // 개별 예약 조회
+    Tuple findReservationAndSeatByReservationIdAndMembershipId(Long memberId, Long membershipId, Long reservationId);
+
+    // 기업 멤버십의 예약 목록 조회
+    List<Tuple> findAllReservationsAndSeatByCompanyMembershipId(Long membershipId, Long memberId);
+
+    // 기업 멤버십 단일 예약 조회
+    Tuple findReservationAndSeatByReservationIdAndCompanyMembershipId(Long memberId, Long companyMembershipId, Long reservationId);
+
+    // 잔여 좌석 조회
+    List<SeatSpace> findAllSeatSpacesByOfficeIdAndType(Long officeId, String type, LocalDateTime start, LocalDateTime end);
 }
